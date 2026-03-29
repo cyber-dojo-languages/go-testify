8cfca73559d4f70003b7fc1d5e9a05f3d2681e33
JonJagger <jon@kosli.com>
2026-03-29 14:00:20 +0100

    Upgrade to 1.26.1, create go/build-cache, move red-amber-green lambda to start-point

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-[![Github Action (main)](https://github.com/cyber-dojo-languages/go-testify/actions/workflows/main.yml/badge.svg)](https://github.com/cyber-dojo-languages/go-testify/actions)
+[![GitHub CI](../../actions/workflows/main.yml/badge.svg)](../../actions/workflows/main.yml)
 
-## display_name="Go 1.23, testify 1.9"
-## image_name="ghcr.io/cyber-dojo-languages/go_testify"
+## display_name="Go 1.26.1, testify 1.9"
+## image_name=[ghcr.io/cyber-dojo-languages/go_testify](https://ghcr.io/cyber-dojo-languages/go_testify)
 
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash -Eeu
 
 mkdir cdl && cd cdl
-go mod init cdl-go-testify
-go get github.com/stretchr/testify
-go mod download all
+
+cat > go.mod << 'EOF'
+module cdl-go-testify
+
+go 1.26.1
+
+require github.com/stretchr/testify v1.11.1
+EOF
+
+# Dummy file so go mod tidy and go build include testify/assert
+cat > dummy.go << 'EOF'
+package cdl
+import _ "github.com/stretchr/testify/assert"
+EOF
+
+# Resolve all deps (updates go.mod with indirect deps and populates go.sum)
 go mod tidy
+
+# Pre-compile testify into a shared build cache accessible by all users
+mkdir /go/build-cache
+GOCACHE=/go/build-cache go build ./...
+chmod -R 777 /go/build-cache
